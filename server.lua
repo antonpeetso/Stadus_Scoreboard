054cf6fbd2e7c12a74e78101dfee4aa7bf39ab14
ElPumpo <samo_kol@hotmail.com>
2018-05-23 19:55:15 +0200

    Initial cleanup

--- server.lua
+++ server.lua
@@ -2,52 +2,40 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-
-
-
 function CountJobs()
+	local EMSConnected = 0
+	local PoliceConnected = 0
+	local TaxiConnected = 0
+	local MekConnected = 0
+	local BilConnected = 0
+	local MaklareConnected = 0
+	local PlayerConnected = 0
 
 	local xPlayers = ESX.GetPlayers()
-
-	EMSConnected = 0
-	PoliceConnected = 0
-	TaxiConnected = 0
-	MekConnected = 0
-	BilConnected = 0
-	MaklareConnected = 0
-	PlayerConnected = 0
-
 	for i=1, #xPlayers, 1 do
 		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+		
+		if xPlayer == nil then next end
 		PlayerConnected = PlayerConnected + 1
 		if xPlayer.job.name == 'ambulance' then
 			EMSConnected = EMSConnected + 1
-		end		
-		if xPlayer.job.name == 'police' then
+		elseif xPlayer.job.name == 'police' then
 			PoliceConnected = PoliceConnected + 1
-		end	
-		if xPlayer.job.name == 'taxi' then
+		elseif xPlayer.job.name == 'taxi' then
 			TaxiConnected = TaxiConnected + 1
-		end
-		if xPlayer.job.name == 'mecano' then
+		elseif xPlayer.job.name == 'mecano' then
 			MekConnected = MekConnected + 1
-		end
-		if xPlayer.job.name == 'cardealer' then
+		elseif xPlayer.job.name == 'cardealer' then
 			BilConnected = BilConnected + 1
-		end
-		if xPlayer.job.name == 'realestateagent' then
+		elseif xPlayer.job.name == 'realestateagent' then
 			MaklareConnected = MaklareConnected + 1
 		end
 	end
 
-		--SetTimeout(2000, CountEMS)
+	return EMSConnected, PoliceConnected, TaxiConnected, MekConnected, BilConnected, MaklareConnected, PlayerConnected
 end
 
 ESX.RegisterServerCallback('stadusrp_getJobsOnline', function(source, cb)
-
-  local xPlayer    = ESX.GetPlayerFromId(source)
-  CountJobs()
-cb(EMSConnected, PoliceConnected, TaxiConnected, MekConnected, BilConnected, MaklareConnected, PlayerConnected)
-
+	cb(CountJobs())
 end)
 

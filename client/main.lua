7be197b7ade296eca52330ebdc53dc6ed8ee7c32
ElPumpo <samo_kol@hotmail.com>
2018-12-30 20:48:41 +0100

    Check IsControlJustReleased() every frame

--- client/main.lua
+++ client/main.lua
@@ -87,7 +87,7 @@ end
 
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(10)
+		Citizen.Wait(0)
 
 		if IsControlJustReleased(0, Keys['F10']) and IsInputDisabled(0) then
 			ToggleScoreBoard()

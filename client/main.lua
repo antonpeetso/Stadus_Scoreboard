18dd7ca61028f1db70fa4ef5cbd1a751a3f8d3f4
ElPumpo <samo_kol@hotmail.com>
2019-03-26 20:58:26 +0100

    Respect sv_maxclients

--- client/main.lua
+++ client/main.lua
@@ -30,7 +30,7 @@ Citizen.CreateThread(function()
 	SendNUIMessage({
 		action = 'updateServerInfo',
 
-		maxPlayers = 32,
+		maxPlayers = GetConvarInt('sv_maxclients', 32),
 		uptime = 'unknown',
 		playTime = '00h 00m'
 	})
